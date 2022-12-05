1a3fdcab2fd878fedae45ebd251a0efc4d61a94c
Pərviz Eminov <eminovperviz@gmail.com>
2022-12-05 22:33:43 +0400

    Add project files.

---
+++ AlgoTraining/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine(IsAnagram("anagram", "nagaram"));
+
+var arrResult = ReplaceElements(new int[] { 1, 121, 343, 25, 66 });
+
+WriteArray(arrResult);
+
+Console.ReadKey();
+
+
+// find duplicates in given array
+bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> list = new(nums);
+
+    return list.Count < nums.Length;
+}
+
+// find given word is Anagram or not
+bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length)
+        return false;
+
+    var tArray = t.AsSpan().ToArray().OrderBy(x => x).ToArray();
+    var sArray = s.AsSpan().ToArray().OrderBy(x => x).ToArray();
+
+    return Enumerable.SequenceEqual(sArray, tArray);
+}
+
+int[] ReplaceElements(int[] arr)
+{
+    int max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            var next = arr[j];
+
+            if (next > max)
+                max = next;
+
+        }
+        arr[i] = max;
+        max = 0;
+    }
+
+    arr[arr.Length - 1] = -1;
+
+    return arr;
+
+}
+
+static void WriteArray(int[] arrResult)
+{
+    Console.WriteLine("[{0}]",string.Join(",", arrResult));
+}
